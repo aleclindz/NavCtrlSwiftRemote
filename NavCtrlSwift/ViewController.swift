142024132caba977258be5b57a6f8e247da4b445
aleclindz <alexander.v.lindsay@gmail.com>
2016-08-09 11:49:22 -0400

    Small commit

--- NavCtrlSwift/ViewController.swift
+++ NavCtrlSwift/ViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var companies: [String]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        companies = ["Apple mobile devices", "Samsung mobile devices"]
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return companies.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
+        cell.textLabel!.text = companies[indexPath.row]
+        return cell
+    }
+    
 }
 
