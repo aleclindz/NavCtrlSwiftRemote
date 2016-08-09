baca24e353e5298b00ec69eb8a298445d41474fe
aleclindz <alexander.v.lindsay@gmail.com>
2016-08-09 10:58:16 -0400

    Initial Commit

---
+++ NavCtrlSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavCtrlSwift
+//
+//  Created by Alexander Lindsay on 8/9/16.
+//  Copyright © 2016 Alexander Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
